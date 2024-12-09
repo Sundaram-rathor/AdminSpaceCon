ea86d2b5f30c4414e221523d1d7c4fe426c59e38
Tanmay0215 <tanmaygupta.0215@gmail.com>
2024-12-09 12:42:27 +0530

    CreateEvent component

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,91 +1,9 @@
-import Navbar from "../components/Navbar";
-import { useState } from "react";
-
 const Admin = () => {
-  const [eventName, setEventName] = useState("");
-  const [eventDescription, setEventDescription] = useState("");
-  const [eventDate, setEventDate] = useState("");
-  const [eventTime, setEventTime] = useState("");
-  const [eventLocation, setEventLocation] = useState("");
-  const [eventImage, setEventImage] = useState("");
-  const [eventLink, setEventLink] = useState("");
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const event = {
-      name: eventName,
-      description: eventDescription,
-      date: eventDate,
-      time: eventTime,
-      location: eventLocation,
-      image: eventImage,
-      link: eventLink,
-    };
-    console.log(event);
-
-    // handle backend here
-  };
-
   return (
     <div>
-      <div className="h-screen w-screen">
-        <Navbar />
-        <div className="flex-col">
-          <label htmlFor="eventDate">Date</label>
-          <input
-            type="date"
-            id="eventDate"
-            value={eventDate}
-            onChange={(e) => setEventDate(e.target.value)}
-          />
-          <label htmlFor="eventTime">Time</label>
-          <input
-            type="time"
-            id="eventTime"
-            value={eventTime}
-            onChange={(e) => setEventTime(e.target.value)}
-          />
-          <label htmlFor="eventName">Name</label>
-          <input
-            type="text"
-            id="eventName"
-            value={eventName}
-            onChange={(e) => setEventName(e.target.value)}
-          />
-          <label htmlFor="eventDescription">Description</label>
-          <textarea
-            id="eventDescription"
-            value={eventDescription}
-            onChange={(e) => setEventDescription(e.target.value)}
-          />
-          <label htmlFor="eventLocation">Location</label>
-          <input
-            type="text"
-            id="eventLocation"
-            value={eventLocation}
-            onChange={(e) => setEventLocation(e.target.value)}
-          />
-          <label htmlFor="eventImage">Image</label>
-          <input
-            type="text"
-            id="eventImage"
-            value={eventImage}
-            onChange={(e) => setEventImage(e.target.value)}
-          />
-          <label htmlFor="eventLink">Link</label>
-          <input
-            type="text"
-            id="eventLink"
-            value={eventLink}
-            onChange={(e) => setEventLink(e.target.value)}
-          />
-          <button type="submit" onClick={handleSubmit}>
-            Create Event
-          </button>
-      </div>
-      </div>
+      Admin Page
     </div>
-  );
-};
+  )
+}
 
-export default Admin;
+export default Admin
